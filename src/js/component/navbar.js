3140ac68b4601b37677d9c622867d0af1c0c2f55
Frederico Rodrigues <frederico2191@gmail.com>
2023-04-12 16:41:01 +0000

    fallback resolved, Search engine linking not yet

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -4,14 +4,41 @@ import starWars from "./starWars.png"
 import "./navbar.css"
 import { Context } from "../store/appContext";
 import Favorites from "./Favorites";
+import { element } from "prop-types";
 
 
 export const Navbar = () => {
 	const {store, actions} = useContext(Context)
-				const optionsList= store.optionsList
-				const planets= store.planets
-				let generatedList= [...store.people.map(el=>el.name),...store.vehicles.map(el=>el.name),...store.planets.map(el=>el.name)]
-				console.log(generatedList, "the generated List")
+	const optionsList= store.optionsList
+	const planets= store.planets
+	const generatedList= [...store.people.map(el=>el.name),...store.vehicles.map(el=>el.name),...store.planets.map(el=>el.name)]
+	console.log(generatedList, "the generated List")
+	const allOptionsForFilterLinking = [...store.people,...store.vehicles,...store.planets]
+
+	console.log("allOptionsForFilterLinking",allOptionsForFilterLinking)
+
+
+	// const clickSearchFunction = (event) => {
+	// 		event.preventDefault();
+	// 		console.log(event.target[0].value)
+	// 	console.log("event", event)
+	// 	// const formData = new FormData(event.target.value);
+	// 	// 	event.preventDefault();
+	// 	// 	for (let [key, value] of formData.entries()) {
+	// 	// 	  console.log(key, value);
+	// 	// 	}
+	// 	  };
+	
+	// const optionClickHandler = () => {
+
+	// }
+
+	const optionLinking = (selectedElement) => {
+		console.log("selectedElement",selectedElement)
+		return <Link className="datalistOption" to={`${selectedElement.type}/${selectedElement.typeImg}/${selectedElement.uid}`}></Link>
+	}
+	
+
 
 
 
@@ -23,18 +50,18 @@ export const Navbar = () => {
 					<span className="navbar-toggler-icon"></span>
 				</button>
 				<div className="navbar-right-controls">
-					<form className="d-flex" role="search">
+					<form className="d-flex" role="search" onSubmit={console.log("submited")}>
 						<input className="form-control"  list="datalistOptions" type="search" placeholder="Type to search" aria-label="Search"></input>
 						<datalist id="datalistOptions">
-							{generatedList.length && generatedList.map((element, index) => {
+							{allOptionsForFilterLinking.length && allOptionsForFilterLinking.map((element, index) => {
 							return (
-							<option key={index} value={element}/>
+								<option key={index} value={element.name} /*onClick={optionLinking(element)}*//>
 							)})}
 						</datalist>
 
-						{/* <Link to={`/${item.type}/${item.typeImg}/${item.uid}`}>
+						{/* <Link to={`/${item.type}/${item.typeImg}/${item.uid}`}> */}
 							<button className="btn btn-outline-primary navbar-form-control" type="submit">Search</button>
-						</Link> */}
+						{/* </Link> */}
 					</form>
 					<Favorites/>
 				</div>
